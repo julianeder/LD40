731ed0bd28b576f41a8b1a0918b28171434284a9
Julian Eder <julianeder33@gmail.com>
2017-12-02 12:27:17 +0100

    added GUI

--- LD40/Assets/Scripts/Population.cs
+++ LD40/Assets/Scripts/Population.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Population : MonoBehaviour {
 
+    public float population = 7000;
+
+    public float Population_Grew_per_sec = 1f;
+
+    private float t;
+
+    public float pop_crit = 8000;
+    public float pop_max = 15000;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if((t-= Time.deltaTime) < 0)
+        {
+            t = 1;
+            population = population * Population_Grew_per_sec;
+        }
+
+
+
 	}
 }
